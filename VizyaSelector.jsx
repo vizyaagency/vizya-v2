8f81dc03bf717abb48d4732d3c94229d09afd0b9
vizyaagency <salasrayan165@gmail.com>
2025-05-16 13:00:01 +0100

    Add files via upload

---
+++ VizyaSelector.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VizyaSelector = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Welcome to Vizya</h1>
+      <p>This is the immersive entry point.</p>
+    </div>
+  );
+};
+
+export default VizyaSelector;
---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import VizyaSelector from "./VizyaSelector";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <VizyaSelector />
+  </React.StrictMode>
+);
